82de5176b00f689e76161155b55cecb392fc950c
vlederer <35729070+vlederer@users.noreply.github.com>
2020-02-02 12:44:56 +0100

    real(PR) funct...

--- Code_Dirichlet_LAST/mod_fonctions.f90
+++ Code_Dirichlet_LAST/mod_fonctions.f90
@@ -10,55 +10,58 @@ Module mod_fonctions
   Implicit None
 
 Contains
+!CT --> tag pour moduler les fct suivant le CAS test
+!(x,y,t) --> coordonnees d'espace et le temps
 
 !---fonction f premier exemple(terme source)---
-Function f1(CT,x,y,t)
-  Integer, Intent(In) :: CT
-  Real(PR), Intent(In) :: x,y,t
-  Real(PR) :: f1
-  If (CT==1) Then
-    f1=2._PR*(y*(1._PR-y)+x*(1._PR-x))
-  Else IF (CT==2) Then
-    f1=sin(x)+cos(y)
-  Else If (CT==3) Then
-    f1=exp(-((x-(Lx/2._PR))**2._PR))*exp(-((y-(Ly/2._PR))**2._PR))&
+Real(PR) Function f1(CT,x,y,t)
+  Integer,  Intent(In) :: CT
+  Real(PR), Intent(In) :: x, y, t
+  
+  If ( CT == 1 ) Then
+    f1 = 2._PR * (y*(1._PR-y)+x*(1._PR-x))
+  Else IF ( CT == 2 ) Then
+    f1 = sin(x) + cos(y)
+  Else If ( CT == 3 ) Then
+    f1 = exp(-((x-(Lx/2._PR))**2._PR)) * exp(-((y-(Ly/2._PR))**2._PR))&
                                             *cos((pi/2._PR)*t)                                          
   End If
 
 End Function f1
 !----------------------------------------------
 
-!---fonction g premier exemple( bords haut/bas)-
-Function g1(CT,x,y,t)
-  Integer, Intent(In) :: CT
+!---fonction g premier exemple( bords haut/bas)-Conditions limites
+Real(PR) Function g1(CT,x,y,t)
+  Integer,  Intent(In) :: CT
   Real(PR), Intent(In) :: x,y,t
-  Real(PR) :: g1
-  If (CT==1) Then
-    g1=0._PR
-  Else IF (CT==2) Then
-    g1=sin(x)+cos(y)
-  Else If (CT==3) Then
-    g1=0._PR
+  
+  If ( CT == 1 ) Then
+    g1 = 0._PR
+  Else IF ( CT == 2 ) Then
+    g1 = sin(x) + cos(y)
+  Else If ( CT == 3 ) Then
+    g1 = 0._PR
   End If
 End Function g1
 !----------------------------------------------
 
-!---function h premier exemple(bord gauche/droite)
-Function h1(CT,x,y,t)
-  Integer, Intent(In) :: CT
+!---function h premier exemple(bord gauche/droite)-Condition Limites
+Real(PR) Function h1(CT,x,y,t)
+  Integer,  Intent(In) :: CT
   Real(PR), Intent(In) :: x,y,t
-  Real(PR) :: h1
-  If (CT==1) Then
-    h1=0._PR
-  Else IF (CT==2) Then
-    h1=sin(x)+cos(y)
-  Else If (CT==3) Then
-    h1=1._PR
+  
+  If ( CT == 1 ) Then
+    h1 = 0._PR
+  Else IF ( CT == 2 ) Then
+    h1 = sin(x) + cos(y)
+  Else If ( CT == 3 ) Then
+    h1 = 1._PR
   End If
 End Function h1
 !---Fonctions donnant les vecteurs Xi,Yj,Tn---------------
 
-
+!Pour discrtiser l'espace et le temps:
+!verision sequentiel
 Subroutine param(X,Y,T)
   Implicit None
   !---variables------------------------- 
@@ -80,14 +83,14 @@ Subroutine param(X,Y,T)
   LBX = lbound(X,1)
   UBX = ubound(X,1)
 End Subroutine param
-
+!version parallele:
 Subroutine param_para(rang,Np,S1,S2,Ny_l,X,Y,T)
   Implicit None
   !---variables-------------------------
-  Integer, Intent(IN)  :: rang, Np, S1, S2, Ny_l 
+  Integer,  Intent(IN)  :: rang, Np, S1, S2, Ny_l 
   real(PR), Dimension(:), Allocatable, Intent(InOut)  :: X
   real(PR), Dimension(:), Allocatable, Intent(InOut)  :: Y
-  real(PR), Dimension(:), Allocatable, Intent(InOut) :: T
+  real(PR), Dimension(:), Allocatable, Intent(InOut)  :: T
   Integer :: i
   !-------------------------------------
   ALLOCATE(Y(0:Ny_l+1),T(0:Nt+1))
@@ -118,20 +121,6 @@ Subroutine param_para(rang,Np,S1,S2,Ny_l,X,Y,T)
   
 End Subroutine param_para
 !-------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
