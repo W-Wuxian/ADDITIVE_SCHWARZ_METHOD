67c9bdce933457ed7431c647eceaa0f4322cf21d
vlederer <35729070+vlederer@users.noreply.github.com>
2020-02-02 13:55:25 +0100

    Update mod_comm.f90

--- DD_Neuman_TRUE_LAST/mod_comm.f90
+++ DD_Neuman_TRUE_LAST/mod_comm.f90
@@ -10,11 +10,7 @@ CONTAINS
     Real(PR), Dimension(it1:itN), Intent(IN):: U
     Real(PR), Dimension(LBUG:UBUG), Intent(INOUT):: UG
     Real(PR), Dimension(LBUD:UBUD), Intent(INOUT):: UD
-!!$    Integer:: A, B, C, D, iD1, iD2, iG1, iG2
-!!$    B = itN - 2*(overlap-1)*Ny_g; A = B - (Ny_g-1)
-!!$    C = it1 +2*(overlap-1)*Ny_g; D = C + (Ny_g-1)
-!!$    iD1 = itN + 1; iD2 = itN + Ny_g
-!!$    iG1 = it1 - Ny_g; iG2 = it1 -1
+
     IF(rang == 0)THEN !                               rang   tag channel
        CALL MPI_SEND(U(A:B),2*Ny_g,MPI_DOUBLE_PRECISION,rang+1,rang+1,MPI_COMM_WORLD,stateinfo)
        CALL MPI_RECV(UD(LBUD:UBUD),2*Ny_g,MPI_DOUBLE_PRECISION,rang+1,rang,MPI_COMM_WORLD,status,stateinfo)
@@ -34,11 +30,7 @@ CONTAINS
     Real(PR), Dimension(it1:itN), Intent(IN):: U
     Real(PR), Dimension(LBUG:UBUG), Intent(INOUT):: UG
     Real(PR), Dimension(LBUD:UBUD), Intent(INOUT):: UD
-!!$    Integer:: A, B, C, D, iD1, iD2, iG1, iG2
-!!$    B = itN - 2*(overlap-1)*Ny_g; A = B - (Ny_g-1)
-!!$    C = it1 +2*(overlap-1)*Ny_g; D = C + (Ny_g-1)
-!!$    iD1 = itN + 1; iD2 = itN + Ny_g
-!!$    iG1 = it1 - Ny_g; iG2 = it1 -1
+
      IF(rang == 0)THEN !                               rang   tag channel
        CALL MPI_SEND(U(A:B),2*Ny_g,MPI_DOUBLE_PRECISION,rang+1,rang+1,MPI_COMM_WORLD,stateinfo)
        CALL MPI_RECV(UD(LBUD:UBUD),2*Ny_g,MPI_DOUBLE_PRECISION,rang+1,rang,MPI_COMM_WORLD,status,stateinfo)
